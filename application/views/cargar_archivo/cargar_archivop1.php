eb1980c3cf9a5e05bed7e20c01c3f86f056e72ef
Gerardelli Santiago Cruz <estivcap@gmail.com>
2022-04-27 07:21:35 -0500

    Se agregaron los controller y vistas para cargar el archivo csv y ver detalle del reporte, estan con la estructura del archivo inicio.hpp, falta modificalos a los prototipos realizados en adobe XD

---
+++ application/views/cargar_archivo/cargar_archivop1.php
@@ -0,0 +1,214 @@
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Módulo de ingresos de SIPAE</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <button type="button" class="btn btn-block btn-success">Nuevo reporte de pagos</button>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-12">
+
+
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">Resumen de Ingresos</h3>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body">
+                <table id="example1" class="table table-bordered table-striped">
+                  <thead>
+                  <tr>
+                    <th>Periodo del reporte</th>
+                    <th>Fecha de carga</th>
+                    <th>Total de pagos EXAVER</th>
+                    <th>Total de pagos de CERTIFICADO</th>
+                    <th>Descripción de carga financiera</th>
+					<th>Detalles</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                  <tr>
+                    <td>Nombre del periodo</td>
+                    <td>10-01-2022</td>
+                    <td>10</td>
+                    <td>4</td>
+                    <td>Descripción del reporte</td>
+					<td><a href="#">Ver detalles del reporte</a></td>
+                  </tr>
+                  <tr>
+                    <td>Nombre del periodo</td>
+                    <td>10-01-2022</td>
+                    <td>12</td>
+                    <td>5</td>
+                    <td>Descripción del reporte</td>
+					<td><a href="#">Ver detalles del reporte</a></td>
+                  </tr>
+				  <tr>
+                    <td>Nombre del periodo</td>
+                    <td>10-01-2022</td>
+                    <td>12</td>
+                    <td>5</td>
+                    <td>Descripción del reporte</td>
+					<td><a href="#">Ver detalles del reporte</a></td>
+                  </tr>
+				  <tr>
+                    <td>Nombre del periodo</td>
+                    <td>10-01-2022</td>
+                    <td>10</td>
+                    <td>4</td>
+                    <td>Descripción del reporte</td>
+					<td><a href="#">Ver detalles del reporte</a></td>
+                  </tr>
+                  <tr>
+                    <td>Nombre del periodo</td>
+                    <td>10-01-2022</td>
+                    <td>12</td>
+                    <td>5</td>
+                    <td>Descripción del reporte</td>
+					<td><a href="#">Ver detalles del reporte</a></td>
+                  </tr>
+				  <tr>
+                    <td>Nombre del periodo</td>
+                    <td>10-01-2022</td>
+                    <td>12</td>
+                    <td>5</td>
+                    <td>Descripción del reporte</td>
+					<td><a href="#">Ver detalles del reporte</a></td>
+                  </tr>
+				  <tr>
+                    <td>Nombre del periodo</td>
+                    <td>10-01-2022</td>
+                    <td>10</td>
+                    <td>4</td>
+                    <td>Descripción del reporte</td>
+					<td><a href="#">Ver detalles del reporte</a></td>
+                  </tr>
+                  <tr>
+                    <td>Nombre del periodo</td>
+                    <td>10-01-2022</td>
+                    <td>12</td>
+                    <td>5</td>
+                    <td>Descripción del reporte</td>
+					<td><a href="#">Ver detalles del reporte</a></td>
+                  </tr>
+				  <tr>
+                    <td>Nombre del periodo</td>
+                    <td>10-01-2022</td>
+                    <td>12</td>
+                    <td>5</td>
+                    <td>Descripción del reporte</td>
+					<td><a href="#">Ver detalles del reporte</a></td>
+                  </tr>
+				  <tr>
+                    <td>Nombre del periodo</td>
+                    <td>10-01-2022</td>
+                    <td>10</td>
+                    <td>4</td>
+                    <td>Descripción del reporte</td>
+					<td><a href="#">Ver detalles del reporte</a></td>
+                  </tr>
+                  <tr>
+                    <td>Nombre del periodo</td>
+                    <td>10-01-2022</td>
+                    <td>12</td>
+                    <td>5</td>
+                    <td>Descripción del reporte</td>
+					<td><a href="#">Ver detalles del reporte</a></td>
+                  </tr>
+				  <tr>
+                    <td>Nombre del periodo</td>
+                    <td>10-01-2022</td>
+                    <td>12</td>
+                    <td>5</td>
+                    <td>Descripción del reporte</td>
+					<td><a href="#">Ver detalles del reporte</a></td>
+                  </tr>
+				  <tr>
+                    <td>Nombre del periodo</td>
+                    <td>10-01-2022</td>
+                    <td>10</td>
+                    <td>4</td>
+                    <td>Descripción del reporte</td>
+					<td><a href="#">Ver detalles del reporte</a></td>
+                  </tr>
+                  <tr>
+                    <td>Nombre del periodo</td>
+                    <td>10-01-2022</td>
+                    <td>12</td>
+                    <td>5</td>
+                    <td>Descripción del reporte</td>
+					<td><a href="#">Ver detalles del reporte</a></td>
+                  </tr>
+				  <tr>
+                    <td>Nombre del periodo</td>
+                    <td>10-01-2022</td>
+                    <td>12</td>
+                    <td>5</td>
+                    <td>Descripción del reporte</td>
+					<td><a href="#">Ver detalles del reporte</a></td>
+                  </tr>
+				  <tr>
+                    <td>Nombre del periodo</td>
+                    <td>10-01-2022</td>
+                    <td>10</td>
+                    <td>4</td>
+                    <td>Descripción del reporte</td>
+					<td><a href="#">Ver detalles del reporte</a></td>
+                  </tr>
+                  <tr>
+                    <td>Nombre del periodo</td>
+                    <td>10-01-2022</td>
+                    <td>12</td>
+                    <td>5</td>
+                    <td>Descripción del reporte</td>
+					<td><a href="#">Ver detalles del reporte</a></td>
+                  </tr>
+				  <tr>
+                    <td>Nombre del periodo</td>
+                    <td>10-01-2022</td>
+                    <td>12</td>
+                    <td>5</td>
+                    <td>Descripción del reporte</td>
+					<td><a href="#">Ver detalles del reporte</a></td>
+                  </tr>               
+                  </tbody>
+                  <tfoot>
+                  <tr>
+                    <th>Periodo del reporte</th>
+                    <th>Fecha de carga</th>
+                    <th>Total de pagos EXAVER</th>
+                    <th>Total de pagos de CERTIFICADO</th>
+                    <th>Descripción de carga financiera</th>
+					<th>Detalles</th>
+                  </tr>
+                  </tfoot>
+                </table>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </div>
+          <!-- /.col -->
+        </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+ 
