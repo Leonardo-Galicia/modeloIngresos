bf2227a3909b6c9af56da5bd9586fc8d0fdc60ff
Leonardo-Galicia <50188761+Leonardo-Galicia@users.noreply.github.com>
2022-04-26 20:09:55 -0500

    Datos

--- application/views/inicio.php
+++ application/views/inicio.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-    <title>Collapsible sidebar using Bootstrap 4</title>
+    <title>Collapsibsjbsbsuble sidebar using Bootstrap 4</title>
 
     <!-- Bootstrap CSS CDN -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
@@ -457,7 +457,7 @@
                 <td>Javascript Developer</td>
                 <td>San Francisco</td>
                 <td>39</td>
-                <td>2009/09/15</td>
+                <td>2008/09/15</td>
                 <td>$205,500</td>
             </tr>
             <tr>
